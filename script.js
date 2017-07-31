5f86f108b3e401d8a30ad2951a7b1367aad5ec83
Rafal Perzyna <anthilles@outlook.com>
2017-07-31 10:26:32 +0200

    Dodano kurs XEM/BTC z api giełdy / JSON

--- script.js
+++ script.js
@@ -3,16 +3,38 @@ nasluch.addEventListener('click', function(){
    pobranie(); 
 });
 
+
 function pobranie(){
     const dane = document.getElementById('wartosc1');  //wyświetla wprowadzoną ilość XEM
-    console.log(dane.value);
+//    console.log(dane.value);
+
     przeliczenieXem();
     function przeliczenieXem(){ // funkcja przeliczająca wprowadzoną wartość XEM względem kursu BTC
         const xemBtc = dane.value * 2;
         const kursXemBtc = document.getElementById("kursXemBtc");
         kursXemBtc.innerHTML = xemBtc;
+        przeliczenieDolary();
+        function przeliczenieDolary(){
+            const btcUsd = xemBtc * 2;
+            const kursBtcUsd = document.getElementById("kursBtcUsd");
+            kursBtcUsd.innerHTML = btcUsd;
+            przeliczeniePln();
+            function przeliczeniePln(){
+                const usdPln = btcUsd * 2;
+                const kursBtcUsd = document.getElementById("wartoscPLN");
+                kursBtcUsd.innerHTML = usdPln;
+            }
+        }
     }
 }
 
 
+function setup() {
+    data = loadJSON("https://poloniex.com/public?command=returnTicker", gotData);
+};
 
+function gotData(data) {
+    const kursXem = data.BTC_XEM.last;
+    const kursXemId = document.getElementById("kurs");
+    kursXemId.innerHTML = kursXem;
+};
